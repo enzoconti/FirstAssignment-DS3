8d538cfd051421a84200fe1669092b1337b2b64b
Jade16 <jade_bortot_paiva@usp.br>
2022-10-29 11:48:06 -0300

    -

--- include/fileRoutines.h
+++ include/fileRoutines.h
@@ -38,7 +38,7 @@ void RRNread(char* filepath, int RRN);
 
 int removeIntOnFile(FILE* fp, int fieldFlag, int key);
 int removeStrOnFile(FILE*fp, int fieldFlag, char* key);
-void removeRegister(FILE *fp);
+void removeRegister(FILE *fp, int count);
 void compact(FILE *fp, int numRemovidos);
 int/*void*/ quantidadeRegistros(FILE *fp);
 
